125b2acc07fb1c5e5d43b683c33ea1fe4109ad0d
densus77 <51282952+densus77@users.noreply.github.com>
2019-10-22 00:25:37 +0700

    Update Setting.php

--- Setting.php
+++ Setting.php
@@ -12,11 +12,11 @@ $banner= "\033[1;31m╔═══════════════════
        ╝╚╝╚═╝ ╩   ╚  ╚═╝╩╚═  ╚═╝╩ ╩╩═╝╚═╝
                                  \033[0;38m『BOT.TERMUX』
 \033[1;31m╔════════════════════════════════════════════════╗
-\033[1;31m║𝖈𝖗𝖊𝖆𝖙𝖊 : TEAM 8                                 ║
-\033[1;31m║𝕾𝖚𝖕𝖔𝖗𝖙 : ALL MEMBER TEAM 8                      ║
-\033[1;31m║𝖈𝖍𝖆𝖓𝖓𝖊𝖑 𝖄𝖙 : TEAM 8                             ║
-\033[1;31m║𝕲𝖗𝖚𝖕 𝖙𝖊𝖑𝖊𝖌𝖗𝖆𝖒 :@jancokau990                     ║
-\033[1;31m║𝕯𝖔𝖓𝖆𝖙𝖎𝖔𝖓𝖘 : DE5KEzYV86qrDoNw7bN8F51Tkd2mbXZjMg  ║
+\033[1;31m║Di produksi oleh : TEAM 8                                ║
+\033[1;31m║Di import oleh   : ALL MEMBER TEAM 8                     ║
+\033[1;31m║Video channel: TEAM 8                                    ║
+\033[1;31m║Telegram grup:@jancokau990                               ║
+\033[1;31m║Tempat Amal  : DE5KEzYV86qrDoNw7bN8F51Tkd2mbXZjMg        ║
 \033[1;31m╚════════════════════════════════════════════════╝
 ";
 
